918e9a24d5e19007f4f8272dd02260325233b15d
charsijawaan <shakeelusama3@gmail.com>
2020-07-01 15:42:30 -0700

    optimized market details table

--- app/js/market-details-div.js
+++ app/js/market-details-div.js
@@ -34,9 +34,14 @@ resetMarketDetailsDiv = () => {
     $('#market-details-div').html(``)
 }
 
-generateMarketTable = () => {    
-    getAllCustomersWithPendingAmount((err, data) => {        
+generateMarketTable = () => {
+
+    getAllCustomersWithPendingAmount((err, data) => {
+
         for(let i = 0; i < data.length; i++) {
+            $(`#view-market-details-table`).append(`
+                <tr id="market-detail-customer-${i}" style="font-weight: bold; font-size: 17px;"><tr>
+            `)
             let record = {
                 'company_name': '--',
                 'customer_name': '--',
@@ -50,77 +55,93 @@ generateMarketTable = () => {
                 '45_kg_cylinders_super': '--'
             }
             getNumberOfCylindersinPossesion(data[i].customer_id, 11, (err,num)=>{
-                if(num[0].plant_id === 1) {
-                    record['11_kg_cylinders_chenab'] = num[0].number_of_cylinders
+                if(num[0] != undefined) {
+                    if(num[0].plant_id === 1) {
+                        record['11_kg_cylinders_chenab'] = num[0].number_of_cylinders
+                    }
+                    if(num[0].plant_id === 2) {
+                        record['11_kg_cylinders_super'] = num[0].number_of_cylinders
+                    }
                 }
-                if(num[0].plant_id === 2) {
-                    record['11_kg_cylinders_super'] = num[0].number_of_cylinders
+                if(num[1] != undefined) {
+                    if(num[1].plant_id === 1) {
+                        record['11_kg_cylinders_chenab'] = num[1].number_of_cylinders
+                    }
+                    if(num[1].plant_id === 2) {
+                        record['11_kg_cylinders_super'] = num[1].number_of_cylinders
+                    }
                 }
-                if(num[1].plant_id === 1) {
-                    record['11_kg_cylinders_chenab'] = num[1].number_of_cylinders
-                }
-                if(num[1].plant_id === 2) {
-                    record['11_kg_cylinders_super'] = num[1].number_of_cylinders
-                }
-            })
-            getNumberOfCylindersinPossesion(data[i].customer_id, 15, (err,num)=>{
-                if(num[0].plant_id === 1) {
-                    record['15_kg_cylinders_chenab'] = num[0].number_of_cylinders
-                }
-                if(num[0].plant_id === 2) {
-                    record['15_kg_cylinders_super'] = num[0].number_of_cylinders
-                }
-                if(num[1].plant_id === 1) {
-                    record['15_kg_cylinders_chenab'] = num[1].number_of_cylinders
-                }
-                if(num[1].plant_id === 2) {
-                    record['15_kg_cylinders_super'] = num[1].number_of_cylinders
-                }
-            })
-            getNumberOfCylindersinPossesion(data[i].customer_id, 45, (err,num)=>{
-                if(num[0].plant_id === 1) {
-                    record['45_kg_cylinders_chenab'] = num[0].number_of_cylinders
-                }
-                if(num[0].plant_id === 2) {
-                    record['45_kg_cylinders_super'] = num[0].number_of_cylinders
-                }
-                if(num[1].plant_id === 1) {
-                    record['45_kg_cylinders_chenab'] = num[1].number_of_cylinders
-                }
-                if(num[1].plant_id === 2) {
-                    record['45_kg_cylinders_super'] = num[1].number_of_cylinders
-                }
+
+                getNumberOfCylindersinPossesion(data[i].customer_id, 15, (err,num)=>{
+                    if(num[0] != undefined) {
+                        if(num[0].plant_id === 1) {
+                            record['15_kg_cylinders_chenab'] = num[0].number_of_cylinders
+                        }
+                        if(num[0].plant_id === 2) {
+                            record['15_kg_cylinders_super'] = num[0].number_of_cylinders
+                        }
+                    }
+                    if(num[1] != undefined) {
+                        if(num[1].plant_id === 1) {
+                            record['15_kg_cylinders_chenab'] = num[1].number_of_cylinders
+                        }
+                        if(num[1].plant_id === 2) {
+                            record['15_kg_cylinders_super'] = num[1].number_of_cylinders
+                        }
+                    }
+
+                    getNumberOfCylindersinPossesion(data[i].customer_id, 45, (err,num)=>{
+                        if(num[0] != undefined) {
+                            if(num[0].plant_id === 1) {
+                                record['45_kg_cylinders_chenab'] = num[0].number_of_cylinders
+                            }
+                            if(num[0].plant_id === 2) {
+                                record['45_kg_cylinders_super'] = num[0].number_of_cylinders
+                            }
+                        }
+                        if(num[1] != undefined) {
+                            if(num[1].plant_id === 1) {
+                                record['45_kg_cylinders_chenab'] = num[1].number_of_cylinders
+                            }
+                            if(num[1].plant_id === 2) {
+                                record['45_kg_cylinders_super'] = num[1].number_of_cylinders
+                            }
+                        }
+
+                        record['company_name'] = titleCase(data[i].company_name)
+                        if(data[i].customer_name != null) {
+                            record['customer_name'] = data[i].customer_name
+                        }
+                        record['total_pending_amount'] = data[i].total_pending_amount
+                        record['limit'] = data[i].limit
+                        let color
+                        if(data[i].total_pending_amount >= data[i].limit) {
+                            color = 'red'
+                        }
+                        else {
+                            color = 'white'
+                        }
+                        $(`#market-detail-customer-${i}`).append(`
+                            <td>${i + 1}</td>
+                            <td>${record['company_name']}</td>
+                            <td>${record['customer_name']}</td>
+                            <td>${record['total_pending_amount']}</td>
+                            <td>${record['11_kg_cylinders_chenab']}</td>
+                            <td>${record['15_kg_cylinders_chenab']}</td>
+                            <td>${record['45_kg_cylinders_chenab']}</td>
+                            <td>${record['11_kg_cylinders_super']}</td>
+                            <td>${record['15_kg_cylinders_super']}</td>
+                            <td>${record['45_kg_cylinders_super']}</td>
+                        `)
+                        $(`#market-detail-customer-${i}`).css('color', color)
+                    })
+                })
             })
 
-            record['company_name'] = titleCase(data[i].company_name)
-            if(data[i].customer_name != null) {
-                record['customer_name'] = data[i].customer_name
-            }            
-            record['total_pending_amount'] = data[i].total_pending_amount
-            record['limit'] = data[i].limit
-            let color
-            if(data[i].total_pending_amount >= data[i].limit) {
-                color = 'red'
-            }
-            else {
-                color = 'white'
-            }
-            setTimeout(()=>{
-                $(`#view-market-details-table`).append(`
-                <tr style="color: ${color}; font-weight: bold; font-size: 17px;">
-                    <td>${i + 1}</td>
-                    <td>${record['company_name']}</td>
-                    <td>${record['customer_name']}</td>
-                    <td>${record['total_pending_amount']}</td>
-                    <td>${record['11_kg_cylinders_chenab']}</td>
-                    <td>${record['15_kg_cylinders_chenab']}</td>
-                    <td>${record['45_kg_cylinders_chenab']}</td>
-                    <td>${record['11_kg_cylinders_super']}</td>
-                    <td>${record['15_kg_cylinders_super']}</td>
-                    <td>${record['45_kg_cylinders_super']}</td>
-                </tr>
-            `)
-            }, 1000)            
+
+
+
         }
+
     })
 }
